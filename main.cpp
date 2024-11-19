a2d559d50bdc299fe40bfb1066a6a4dbde47b793
a <markusus123765@gmail.com>
2024-11-19 17:48:56 +0100

    Utworzenie początkowego algorytmu do zrealizowania zadania

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 int main() {
     int a;
+    int c[4];
     cout << "podaj rozmiar tablicy: "<< endl;
     cin >> a;
+    int tab[a];
+    for(int i= 0;i <a; i++) {
+     cout << "podaj wartosci tablicy : " << endl;
+        cin >> tab[i];
+    }
+    for(int i = 0; i < a; i++) {
+        cout << tab[i] << " ";
+    }
+    for(int i = 0; i < a; i++) {
+    switch(tab[i]>tab[i+1]) {
+            case
+    }
+    }
+cout << "najdluzszy malejacy podciag to: " << endl;
     return 0;
 }
