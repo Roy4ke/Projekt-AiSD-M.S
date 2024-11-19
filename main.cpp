234f6a8d846a5b33911ec229b6bfffce78a21406
a <markusus123765@gmail.com>
2024-11-19 19:14:04 +0100

    praca nad algorytmem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-int main() {
+void najdluzszypodciag(int A[], int n) {
+    if(n==0)return;
+    int* dlugosc = new int[n];      // Tablica do przechowywania długości najdłuższego malejącego podciągu
+    int* popr = new int[n];        // Tablica do przechowywania poprzedników w podciągu
+    int maxdlugosc = 1;             // Maksymalna długość znalezionego podciągu
+    int* indk = new int[n];  // Tablica przechowująca indeksy końcowe podciągów o długości maxLength
+    int ilosc = 0;              //ilości podciągów
+    for (int i = 0; i < n; i++) { // tablice pomocnicze do wyznaczania długości podciągu
+        dlugosc[i] = 1;
+        popr[i] = -1;
+    }
+}
+int main(){
     int a;
     int c[4];
     cout << "podaj rozmiar tablicy: "<< endl;
@@ -13,11 +25,7 @@ int main() {
     for(int i = 0; i < a; i++) {
         cout << tab[i] << " ";
     }
-    for(int i = 0; i < a; i++) {
-    switch(tab[i]>tab[i+1]) {
-            case
-    }
-    }
+
 cout << "najdluzszy malejacy podciag to: " << endl;
     return 0;
 }
